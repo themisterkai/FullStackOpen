bcb18f3525eb77dcfc7b26e2991b361162d0825b
Kris Albert Lee <krisalbert@gmail.com>
2024-10-24 14:09:53 +0200

    add dist to backend

--- part1/notes-backend/index.js
+++ part1/notes-backend/index.js
@@ -34,10 +34,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'));
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello Kai + Ilya + Soma !!!</h1>');
-});
-
 app.get('/api/notes', (request, response) => {
   response.json(notes);
 });
