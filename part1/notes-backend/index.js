1cd4c0f9879cc4ac250a9fc4a2baf6e55beada43
Kris Albert Lee <krisalbert@gmail.com>
2024-10-24 14:07:07 +0200

    add part 3

---
+++ part1/notes-backend/index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+let notes = [
+  {
+    id: '1',
+    content: 'HTML is easy',
+    important: true,
+  },
+  {
+    id: '2',
+    content: 'Browser can execute only JavaScript',
+    important: false,
+  },
+  {
+    id: '3',
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
+];
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'application/json' });
+//   response.end(JSON.stringify(notes));
+// });
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('dist'));
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello Kai + Ilya + Soma !!!</h1>');
+});
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  const note = notes.find(note => note.id === id);
+  if (note != null) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter(note => note.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
