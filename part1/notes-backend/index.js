d6511802e149766295084fd8101740b47406f28b
Kris Albert Lee <krisalbert@gmail.com>
2024-10-25 13:04:16 +0200

    add linters

--- part1/notes-backend/index.js
+++ part1/notes-backend/index.js
@@ -62,7 +62,7 @@ app.post('/api/notes', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end();
     })
     .catch(error => next(error));
