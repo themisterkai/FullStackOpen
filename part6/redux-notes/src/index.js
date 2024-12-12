61d99225215e8045f0953848724a8aa9d1e73c2b
Kris Albert Kai Lee <kai@hellokai.dev>
2024-12-12 09:30:46 +0100

    part 6

---
+++ part6/redux-notes/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import App from './App';
+import noteReducer from './reducers/noteReducer';
+import filterReducer from './reducers/filterReducer';
+
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
+});
+
+// store.dispatch({
+//   type: 'NEW_NOTE',
+//   data: {
+//     content: 'the app state is in redux store',
+//     important: true,
+//     id: 1
+//   }
+// })
+
+// store.dispatch({
+//   type: 'NEW_NOTE',
+//   data: {
+//     content: 'state changes are made with actions',
+//     important: false,
+//     id: 2
+//   }
+// })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
