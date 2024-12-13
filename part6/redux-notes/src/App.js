e63608154f35524341e97018ace4e64677e442c1
Kris Albert Kai Lee <kai@hellokai.dev>
2024-12-13 13:02:37 +0100

    part 6: use db.json

--- part6/redux-notes/src/App.js
+++ part6/redux-notes/src/App.js
@@ -1,11 +1,16 @@
+import { useDispatch } from 'react-redux';
 import NewNote from './components/NewNote';
 import Notes from './components/Notes';
 import VisibilityFilter from './components/VisibilityFilter';
+import { useEffect } from 'react';
+import noteService from './services/notes';
+import { setNotes } from './reducers/noteReducer';
 
 const App = () => {
-  const filterSelected = value => {
-    console.log(value);
-  };
+  const dispatch = useDispatch();
+  useEffect(() => {
+    noteService.getAll().then(notes => dispatch(setNotes(notes)));
+  }, []);
   return (
     <div>
       <NewNote />
--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import App from './App';
-import noteReducer from './reducers/noteReducer';
+import noteReducer, { setNotes } from './reducers/noteReducer';
 import filterReducer from './reducers/filterReducer';
 
 const store = configureStore({
@@ -30,6 +30,7 @@ const store = configureStore({
 //     id: 2
 //   }
 // })
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
